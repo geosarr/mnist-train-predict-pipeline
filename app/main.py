a0fee5473144495d659ac64d07ac1649b74133a7
Georges SARR <georges_sarr@carrefour.com>
2022-07-13 14:05:54 +0200

    added FileResponse

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-from train import train
 from utils import run_train, load_dataset, run_predict
 
 
@@ -15,12 +14,10 @@ def training(n_train:int, batch_size:int, n_epochs:int, n_early_stop:int, save_l
     train_loader, val_loader = load_dataset(n_train, batch_size)
     train_losses, val_losses = run_train(n_epochs=n_epochs, train_loader=train_loader, val_loader=val_loader, 
                                         batch_size=batch_size, n_train=n_train, n_early_stop=n_early_stop, save_losses=save_losses)
-    return {"train_losses":train_losses, 
-            "val_losses":val_losses} 
-
+    return FileResponse(path="./results/losses.png", filename="losses.png", media_type="")
 
 @app.get("/prediction")
 def prediction(run: bool):
     if run:
         run_predict()
-        # return FileResponse(path="./results/predictions.txt", filename="predictions.txt", media_type='text/plain')
+        return FileResponse(path="./results/predictions.txt", filename="predictions.txt", media_type='text/plain')
