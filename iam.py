cb7589d1a5a6056b4a99fe22dfb80ce970c4a3a3
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-11-13 18:06:37 +0100

    added scope exception

--- iam.py
+++ iam.py
@@ -7,8 +7,8 @@ from fastapi import Depends
 from jose import JWTError, jwt
 from jose.exceptions import ExpiredSignatureError
 import sqlite3 as sql
-from config import DATA_DIR, PWD_CONTEXT, SECRET_KEY, ALGORITHM
-from config import OAUTH2_SCHEME, CREDENTIALS_EXCEPTION, EXPIRATION_EXCEPTION
+from config import DATA_DIR, PWD_CONTEXT, SECRET_KEY, ALGORITHM, OAUTH2_SCHEME
+from config import  CREDENTIALS_EXCEPTION, EXPIRATION_EXCEPTION, SCOPE_EXCEPTION
 
 
 class User(BaseModel):
@@ -20,6 +20,7 @@ class User(BaseModel):
 
 class UserInDB(User):
     hashed_password: str
+    scopes: dict
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
@@ -29,16 +30,24 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def get_user(cursor: sql.Cursor, username: str) -> Union[UserInDB, None]:
     db_user = cursor.execute(
         f"""
-        SELECT username, hashed_password FROM users
+        SELECT username, hashed_password, training , prediction 
+        FROM users
         WHERE username = '{username}'
     """
     ).fetchall()
     if len(db_user) == 1:
-        db_user = {"username": db_user[0][0], "hashed_password": db_user[0][1]}
+        db_user = {
+            "username": db_user[0][0], 
+            "hashed_password": db_user[0][1],
+            "scopes": {
+                "training": db_user[0][2],
+                "prediction": db_user[0][3]
+            }        
+        }
         return UserInDB(**db_user)
 
 
-def authenticate_user(cursor, username: str, password: str) -> Union[bool, None]:
+def authenticate_user(cursor, username: str, password: str) -> Union[bool, UserInDB]:
     user = get_user(cursor, username)
     if not user:
         return False
@@ -58,12 +67,15 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     return encoded_jwt
 
 
-def is_valid_token(token: str = Depends(OAUTH2_SCHEME)) -> bool:
+def is_valid_token(endpoint: str, token: str = Depends(OAUTH2_SCHEME)) -> bool:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
+        scopes: dict = payload.get("scopes")
         if username is None:
             raise CREDENTIALS_EXCEPTION
+        if scopes[endpoint] != "run":
+            raise SCOPE_EXCEPTION
     except ExpiredSignatureError as exc1:
         raise EXPIRATION_EXCEPTION from exc1
     except JWTError as exc2:
@@ -88,13 +100,13 @@ def create_data_user_database() -> None:
 
         cur.execute(
             """CREATE TABLE users 
-                    (username text, hashed_password text)"""
+                    (username text, hashed_password text, training text, prediction text)"""
         )
 
         cur.execute(
             """INSERT INTO users VALUES
-                    ("johndoe","$2b$12$4FdZ1IT6QarM5CyQ7DEx.e/pwLQQGBpVQLgWVyMs3DnkAHTCOys6W"),
-                    ("alice", "$2b$12$UqWFgNQXSGDFsQs6fnHjTeCcPfa1eSGGVrBSpgk4/X/s/aaN/O6Hy")"""
+                    ("johndoe","$2b$12$4FdZ1IT6QarM5CyQ7DEx.e/pwLQQGBpVQLgWVyMs3DnkAHTCOys6W", ' ', "run"),
+                    ("alice", "$2b$12$UqWFgNQXSGDFsQs6fnHjTeCcPfa1eSGGVrBSpgk4/X/s/aaN/O6Hy", "run", "run")"""
         )
 
         con.commit()
