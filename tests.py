d4c0afb5242a9a35eac925cd21d2f08b6d08e38c
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-11-15 14:28:19 +0100

    added app testing

--- tests.py
+++ tests.py
@@ -1,9 +1,10 @@
 import unittest
 from utils import load_dataset
-
+from app.main import app
+from fastapi.testclient import TestClient
 
 class TestUtils(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.n_train = 40025
         self.batch_size = 64
 
@@ -12,3 +13,13 @@ class TestUtils(unittest.TestCase):
         q, r = self.n_train // self.batch_size, self.n_train % self.batch_size
         assert len(train) == q + (r > 0)
         assert len(val) == 1
+
+class TestApp(unittest.TestCase):
+    def setUp(self) -> None:
+        self.client = TestClient(app)
+
+
+    def test_post_token(self):
+        # response = (self.client).post("/token") 
+        assert 1==1
+
