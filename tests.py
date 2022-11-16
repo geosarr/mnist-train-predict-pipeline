cc6550668a1f15d3daf03d7ec7c82ba4c4f0e0ac
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-11-16 12:42:58 +0100

    added assertion TestApp

--- tests.py
+++ tests.py
@@ -1,10 +1,11 @@
 import unittest
+import json
 from utils import load_dataset
 from app.main import app
 from fastapi.testclient import TestClient
 
 class TestUtils(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         self.n_train = 40025
         self.batch_size = 64
 
@@ -15,11 +16,35 @@ class TestUtils(unittest.TestCase):
         assert len(val) == 1
 
 class TestApp(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         self.client = TestClient(app)
+        self.data_john = {
+                "username": "johndoe", 
+                "password": "very_secret"
+            }
+        self.data_fake = {
+            "username": "bozo",
+            "password": "very_bad"
+        }
 
 
     def test_post_token(self):
-        # response = (self.client).post("/token") 
-        assert 1==1
+        response0 = self.client.post("/token", data=self.data_john)
+        response1 = self.client.post("/token", data=self.data_fake)
+        # good credentials
+        assert response0.status_code == 200
+        assert len(json.loads(response0.text)["access_token"].split(".")) == 3 # JWT format
+        # bad credentials
+        assert response1.status_code == 401
+        assert json.loads(response1.text)["detail"] == "Incorrect username or password"
 
+# import json
+# a = TestClient(app)
+# r = a.post(
+#     "/token", 
+#     data = {
+#         "username": "johndoe", 
+#         "password": "very_secret"
+#     }
+# ) 
+# print(json.loads(r.text))
